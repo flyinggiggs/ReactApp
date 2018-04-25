d25812bb8ae90af6b678dbb5b85a6cf835622342
Jason Shin <flyinggiggs@gmail.com>
2018-04-24 23:39:28 -0400

    React component State with setState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-const App = () => <h1>Hi</h1>
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state ={
+      txt: 'this is the state txt',
+      cat: 0
+    }
+  }
+  update(e){
+    this.setState({txt: e.target.value})
+  }
+  render(){
+    return (
+    <div>
+      <input type="text" onChange={this.update.bind(this)} />
+     <h1>{this.state.txt} - {this.state.cat}</h1>
+    </div>
+    )  
+  }
+}
+
+App.propTypes ={
+  txt: PropTypes.string,
+  cat: PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: "this is the default txt"
+}
 
 export default App
